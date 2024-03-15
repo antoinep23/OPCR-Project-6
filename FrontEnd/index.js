d770c643571f0e10a9c7284ddfcea4b54ccb0e63
Antoine <141731510+antoinep23@users.noreply.github.com>
2024-03-15 11:20:42 +0000

    Init folder + fetch data from API in frontend/index.js

---
+++ FrontEnd/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const gallery = document.querySelector('.gallery')
+gallery.innerHTML = ''
+
+let figure = ''
+const data = []
+
+const dataWorks = async () => {
+    return await fetch('http://localhost:5678/api/works')
+        .then((res) => res.json())
+        .then((data) => data);
+}
+
+dataWorks()
+    .then(works => {
+        for (const work of works) data.push(work)
+    })
+    .then(() => {
+        for (let i = 0; i < data.length; i++) {
+            figure += `<figure><img src='${data[i].imageUrl}' alt='${data[i].title}'><figcaption>${data[i].title}</figcaption></figure>`
+        }
+        gallery.innerHTML = figure
+        console.log(figure)
+    }) 
+
